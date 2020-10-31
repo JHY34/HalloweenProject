bdc6db98fad7d68485be23d9a3f4bbb701a873a6
JHY34 <hamityilmaz34@gmail.com>
2020-10-31 10:57:58 -0500

    second commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,6 @@
 public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
+        System.out.println("This is an additional changes after the initial commit");
     }
 }
